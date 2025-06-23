90cd84184b3a6e6afbc7762aeb80402c26444431
urbruder <harshsingh310105@gmail.com>
2025-06-24 01:56:47 +0530

    Making Navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,10 +9,12 @@ import Product from './pages/Product'
 import Contact from './pages/contact'
 import Cart from './pages/Cart'
 import PlaceOrder from './pages/PlaceOrder'
+import Navbar from './components/Navbar'
+
 const App = () => {
   return (
     <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
-      
+    <Navbar/>
            <Routes>
             <Route path='/' element={<Home/>}/>
             <Route path='/collection' element={<Collection/>}/>
---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,53 @@
+import React from 'react'
+import {Link, NavLink} from 'react-router-dom'
+import { assets } from '../assets/assets'
+
+
+const Navbar = () => {
+  return (
+    <div className='flex items-center justify-between py-5 font-medium'>
+        
+        <img src={assets.real_logo} className='w-36'alt="" />
+        
+        <ul className='hidden sm:flex gap-5 text-sm text-gray-700' >
+          {/* hidden: Applies display: none by default (on all screen sizes) sm:flex: Overrides that and applies display: flex only on small screens and up (≥ 640px) */}
+            <NavLink to='/' className='flex flex-col items-center gap-1'>
+                <p>HOME</p>
+                <hr className='w-2/4 border-none h-[1.5px] bg-gray-700 hidden'/>
+            </NavLink>
+            <NavLink to='/collection' className='flex flex-col items-center gap-1'>
+                <p>COLLECTION</p>
+                <hr className='w-2/4 border-none h-[1.5px] bg-gray-700 hidden'/>
+            </NavLink>
+            <NavLink to='/about' className='flex flex-col items-center gap-1'>
+                <p>ABOUT</p>
+                <hr className='w-2/4 border-none h-[1.5px] bg-gray-700 hidden'/>
+            </NavLink>
+            <NavLink to='/contact' className='flex flex-col items-center gap-1'>
+                <p>CONTACT</p>
+                <hr className='w-2/4 border-none h-[1.5px] bg-gray-700 hidden'/>
+            </NavLink>
+        </ul>
+        <div className='flex items-center gap-6'>
+            <img src={assets.search_icon} className='w-5 cursor-pointer' alt="" />
+            <div className='group relative'>
+              <img src={assets.profile_icon} className='w-5 cursor-pointer' alt="" />
+              <div className='group-hover:block hidden absolute dropdown-menu right-0 pt-4'>
+                <div className='flex flex-col gap-2 w-36 py-3 px-5 bg-slate-100 text-gray-500 rounded'>
+                 <p className='cursor-pointer hover:text-black'> My Profile</p>
+                 <p className='cursor-pointer hover:text-black'> Orders</p>
+                 <p className='cursor-pointer hover:text-black'> Logout</p>
+                </div>
+              </div>
+            </div>
+            <Link to='/cart' className='relative'>
+                <img src={assets.cart_icon} className='w-5 min-w-5' alt="" />
+                <p className='absolute right-[-5px] bottom-[-5px] w-4 text-center leading-4 bg-black text-white aspect-square rounded-full text-[8px]' >10</p>
+            </Link>
+        </div>
+      
+    </div>
+  )
+}
+
+export default Navbar
