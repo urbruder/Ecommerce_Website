1ef0e4fdc9af952fad6462a22401386172605397
urbruder <harshsingh310105@gmail.com>
2025-06-23 23:39:06 +0530

    Adding a new Logo

--- frontend/src/assets/assets.js
+++ frontend/src/assets/assets.js
@@ -73,7 +73,7 @@ import contact_img from './contact_img.png'
 import razorpay_logo from './razorpay_logo.png'
 import stripe_logo from './stripe_logo.png'
 import cross_icon from './cross_icon.png'
-
+import real_logo from './Reallogo.png'
 export const assets = {
     logo,
     hero_img,
@@ -92,7 +92,8 @@ export const assets = {
     contact_img,
     razorpay_logo,
     stripe_logo,
-    cross_icon
+    cross_icon,
+    real_logo
 }
 
 export const products = [
