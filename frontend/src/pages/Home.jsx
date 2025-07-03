de214f735e47a6ede36e3d5e3a298a47c961f465
urbruder <harshsingh310105@gmail.com>
2025-07-04 01:09:43 +0530

    NewsLetterBox and Footer section added.

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,6 +3,8 @@ import Hero from '../components/Hero'
 import LatestCollection from '../components/LatestCollection'
 import BestSeller from '../components/BestSeller'
 import OurPolicy from '../components/OurPolicy'
+import NewsletterBox from '../components/NewsletterBox'
+import Footer from '../components/Footer'
 
 const Home = () => {
   return (
@@ -11,6 +13,8 @@ const Home = () => {
       <LatestCollection/>
       <BestSeller/>
       <OurPolicy/>
+      <NewsletterBox/>
+      <Footer/>
     </div>
   )
 }
