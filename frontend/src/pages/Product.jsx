64301c371ea98e32db2959f8a8aea99e1c013b95
urbruder <harshsingh310105@gmail.com>
2025-07-15 01:27:36 +0530

    Added Product page, more changes required

--- frontend/src/pages/Product.jsx
+++ frontend/src/pages/Product.jsx
@@ -1,11 +1,78 @@
-import React from 'react'
+import React, { useContext,useState,useEffect } from 'react'
+import { ShopContext } from '../context/ShopContext'
+import { useParams } from 'react-router-dom'
+import { assets } from '../assets/assets'
 
 const Product = () => {
-  return (
-    <div>
+  //useParams is a hook that returns an object of key/value pairs of the dynamic params from the current URL
+  //In routes, we can define dynamic segments in the path using a colon (:) before the segment name
+  // so we have used path='/product/:productId' in App.jsx. This will allow us to access the productId from the URL
+const {productId}=useParams();
+const {products,currency}=useContext(ShopContext);
+const [productData,setProductData]=useState(false)
+// productData will hold the data of the product that we want to display
+const [image,setImage]=useState('');
+//Async function to fetch product data based on productId
+// This function will be called when the component mounts
+//it will match the productId from the URL with the productId from the products array
+const fetchProductData= async ()=>{
+  products.map((item)=>{
+    if(item._id==productId){
+      setProductData(item);
+      setImage(item.image[0]);
+       return null;
+    }
+    //Null check to ensure productData is set before rendering
+  }) 
+
+}
+// useEffect hook to call fetchProductData when the component mounts or when productId or products change
+useEffect(()=>{
+  fetchProductData();
+},[productId,products])
+  return productData? (
+    <div className='border-t-2 pt-10 transition-opacity ease-in duration-500 opacity-100'>
+      {/*.............................. Product Data..................................... */}
+      <div className='flex gap-12 sm:gap-12 flex-col sm:flex-row'>
+        {/* ............................Product Images................................... */}
+        <div className='flex-1 flex flex-col-reverse gap-3 sm:flex-row'>
+          <div className='flex sm:flex-col overflow-x-auto sm:overflow-y-scroll justify-between sm:justify-normal sm:w-[18.7%] w-full'>
+            {productData.image.map((item,index)=>(
+              <img onClick={()=>setImage(item)} src={item} key={index} className='w-[24%] sm:w-full sm:mb-3 flex-shrink-0 cursor-pointer' alt="" />
+            ))
+            }
+          </div>
+          <div className='w-full sm:w-[80%]'>
+             <img src={image} className='w-full h-auto' alt="" />
+          </div>
+        </div>
+        {/*...................... Product details............................ */}
+        <div className='flex-1'>
+             <h1 className='font-medium text-2xl mt-2'>{productData.name}</h1>
+             <div className='flex items-center gap-1 mt-2'>
+              <img src={assets.star_icon} alt="" className="w-3 5" />
+              <img src={assets.star_icon} alt="" className="w-3 5" />
+              <img src={assets.star_icon} alt="" className="w-3 5" />
+              <img src={assets.star_icon} alt="" className="w-3 5" />
+              <img src={assets.star_dull_icon} alt="" className="w-3 5" />
+              <p className='pl-2'>(122)</p>
+             </div>
+             <p className='mt-5 text-3xl font-medium'>{currency}{productData.price}</p>
+             <p className='mt-5 text-gray-500 md:w-4/5'>{productData.description}</p>
+             <div className='flex flex-col gap-4 my-8'>
+              <p>Select Size</p>
+              <div className='flex gap-2'>
+                  {productData.sizes.map((item,index)=>(
+                    <button className={`border py-2 px-4 bg-gray-100`} key={index}>{item}</button>
+                  ))}
+              </div>
+
+             </div>
+        </div>
+      </div>
       
     </div>
-  )
+  ): <div className='opacity-0'></div>
 }
 
 export default Product
