bf7242967531e41a550b309cab5e5aede902511a
urbruder <harshsingh310105@gmail.com>
2025-08-16 22:03:02 +0530

    Setting up tailwind, Creating folders and setting up the structure for admin panel

---
+++ admin/src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import {assets} from "../assets/assets";
+
+const Navbar = () => {
+  return (
+    <div>
+      <img src={assets.Reallogo} alt="" />
+      <button>Logout</button>
+    </div>
+  )
+}
+
+export default Navbar
