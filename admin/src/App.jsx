82a9feeb3782f18d38f70cf548aa6c9d17757251
urbruder <harshsingh310105@gmail.com>
2025-08-20 01:08:19 +0530

    Added the authentication for login on admin panel

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Navbar from './components/Navbar'
 import Sidebar from './components/Sidebar'
 import { Route, Routes } from 'react-router-dom'
@@ -6,25 +6,36 @@ import Add from './pages/Add'
 import List from './pages/List'
 import Orders from './pages/Orders'
 import Login from './components/Login'
-
-
+ import { ToastContainer } from 'react-toastify';
+  
+export const backendUrl=import.meta.env.VITE_BACKEND_URL
 const App = () => {
-
-  const [token, setToken] = useState('')
+//Use of this token is to maintain the login state of the admin
+//If the token is present in localStorage, it means the admin is logged in
+//Token means the JWT token received from the backend after successful login
+  const [token, setToken] = useState(localStorage.getItem('token')? localStorage.getItem('token'):'')
+//This checks if the token is present in localStorage when the component mounts
+//If the token is present, it sets the token state to the value from localStorage
    
+useEffect(()=>{
+  localStorage.setItem('token',token);
+},[token])
+
   return (
     <div className='bg-gray-50 min-h-screen'>
-      {token === '' ? <Login /> :
+      <ToastContainer/>
+      {/* This component is used to show toast notifications */}
+      {token === '' ? <Login setToken={setToken} /> :
         <>
-          <Navbar />
+          <Navbar setToken={setToken} />
           <hr />
           <div className='flex w-full'>
             <Sidebar />
             <div className='w-[70%] mx-auto ml-[max(5vw,25px)] my-8 text-gray-600 text-base'>
               <Routes>
-                <Route path='/add' element={<Add />} />
-                <Route path='/list' element={<List />} />
-                <Route path='/orders' element={<Orders />} />
+                <Route path='/add' element={<Add token={token} />} />
+                <Route path='/list' element={<List token={token} />} />
+                <Route path='/orders' element={<Orders token={token} />} />
               </Routes>
             </div>
           </div>
--- admin/src/components/Login.jsx
+++ admin/src/components/Login.jsx
@@ -1,13 +1,25 @@
 import React from 'react'
+import axios from 'axios'
+import { useState } from 'react'
+import { backendUrl } from '../App';
+import { toast } from 'react-toastify';
 
-const Login = () => {
+const Login = ({setToken}) => {
  const [email, setEmail] =useState('');
 const [password, setPassword] =useState('');
     const onSubmitHandler =async(e)=>{
         try {
             e.preventDefault();
+           const response= await axios.post(backendUrl + '/api/user/admin',{email,password});
+           if(response.data.success){
+            setToken(response.data.token);
+           }
+           else{
+               toast.error(response.data.message)
+           }
         } catch (error) {
-            
+            console.log(error);
+            toast.error(error.message)
         }
     }
 
@@ -19,11 +31,11 @@ const [password, setPassword] =useState('');
         <form onSubmit={onSubmitHandler}>
             <div className='mb-3 min-w-72'>
                 <p className='text-sm font-medium text-gray-700 mb-2'>Email Address</p>
-                <input onChange={(e)=>setEmail(e.target.value)} value={email} className='ounded-md w-full px-3 py-2 border border-gray-300 outline-none' type="email" placeholder='your@email.com' required />
+                <input onChange={(e)=>setEmail(e.target.value)} value={email} className='rounded-md w-full px-3 py-2 border border-gray-300 outline-none' type="email" placeholder='your@email.com' required />
             </div>
               <div className='mb-3 min-w-72'>
                 <p className='text-sm font-medium text-gray-700 mb-2'>Password</p>
-                <input onChange={(e)=>setPassword(e.target.value)} value={password} className='ounded-md w-full px-3 py-2 border border-gray-300 outline-none' type="password" placeholder='Enter your password' required />
+                <input onChange={(e)=>setPassword(e.target.value)} value={password} className='rounded-md w-full px-3 py-2 border border-gray-300 outline-none' type="password" placeholder='Enter your password' required />
             </div>
             <button className='mt-2 w-full py-2 px-4 rounded-md text-white bg-black' type='submit'>Login</button>
         </form>
